9f2ded1c7a92f1e7d1a4507d6c52ad527c63ecdc
Mouzzli <cokes03@yandex.ru>
2022-09-13 23:46:37 +0300

    1. Модульные тесты. [#159435 #342800]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -15,7 +15,7 @@ public class ConverterTest {
     }
 
     @Test
-    public void whenConvert200RblThen3dot3333Dlr() {
+    public void whenConvert180RblThen3Dlr() {
         float in = 180;
         float expected = 3;
         float out = Converter.rubleToDollar(in);
